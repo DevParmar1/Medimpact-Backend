9c4ab44c1ef8526f4e48ab4464825d2deaf38409
Dev Parmar <devparmar37@gmail.com>
2021-12-11 02:05:16 +0530

    Feat: MedImpact backend phase-1

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -2,12 +2,19 @@ const routes = require("express").Router();
 const {
 	Login,
     Register,
+	addDetails,
 	verify
 } = require("../Controllers/Auth/AuthController");
+const {authMiddleware} = require("../Middleware/Auth")
 
 routes.get(
 	"/verify/:token",
 	 verify
+)
+routes.post(
+	"/addDetails",
+	authMiddleware,
+	addDetails
 )
 routes.post(
 	"/register",
