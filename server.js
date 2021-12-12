344f850916b71b1c720608e7d2c80e4d71353804
Dev Parmar <devparmar37@gmail.com>
2021-12-12 16:14:03 +0530

    Feat: MedImpact Phase-3

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const app = require("./app");
+var cron = require("node-cron");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+const {donorUpdate, medicineUpdate} = require("./Controllers/Subscription/SubscriberController")
+
+cron.schedule('0 */6 * * *', async () => {
+    medicineUpdate()
+});
+
+cron.schedule('0 */12 * * *', () => {
+    donorUpdate()
+});
 
 app.listen(PORT,(req,res)=>{
     console.log(`Server started at port ${PORT}`)
