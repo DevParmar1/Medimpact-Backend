7940a6afb9c6230656aa524773c2745b7b181b8b
Dev Parmar <devparmar37@gmail.com>
2022-01-02 11:12:31 +0530

    Elastic Beanstalk

---
+++ services/donorUpdate.js
@@ -0,0 +1,20 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const { google } = require("googleapis");
+
+const CLIENT_ID = process.env.ClientAuthId;
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const CLIENT_SECRET = process.env.ClientSecret;
+const REFRESH_TOKEN = process.env.GoogleRefresh;
+
+
+const Oauth2Client = new google.auth.OAuth2(
+	CLIENT_ID,
+	CLIENT_SECRET,
+	REDIRECT_URI
+);
+Oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+exports.donorUpdate = async (req,res) => {
+    
+}
